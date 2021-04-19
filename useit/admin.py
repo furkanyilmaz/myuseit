91b34dd8bff3c603765be02164ff28298fe5b7ac
Furkan <furkanyilmaz-@hotmail.com>
2021-04-19 10:21:17 +0300

    many2many

--- useit/admin.py
+++ useit/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+#from .models import AbstractUser
 
 # Register your models here.
-from useit.models import Tool, Goodside, Badside
+from useit.models import Tool, Goodside, Badside,User
 
+admin.site.register(User)
 admin.site.register(Tool)
 admin.site.register(Goodside)
 admin.site.register(Badside)
+
+
--- useit/models.py
+++ useit/models.py
@@ -1,21 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+#from django.contrib.auth.base_user import BaseUserManager
 
-class Tool (models.Model):
+"""class UserManager(BaseUserManager):
+    use_in_migrations = True
 
-    name = models.CharField(max_length= 50, verbose_name="tool name", blank=True, null= False, unique=True,
+    class Meta:
+        abstract = True
+
+
+    def _create_user(self, email, password):
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user """
+
+
+class User(models.Model):
+    name = models.CharField(max_length=40, unique=True,blank= True,)
+
+
+    def __str__(self):
+        return self.name
+
+class Tool(models.Model):
+    #user = models.ForeignKey(User, on_delete=models.PROTECT, null= True)  # the owner
+    name = models.CharField(max_length=50, verbose_name="tool name", blank=True, null=False, unique=True,
                             editable=True)
     content = models.TextField(verbose_name="ürün açıklaması", max_length=500)
     created = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
     website = models.URLField(max_length=200)
+    users = models.ManyToManyField(User, blank=True)
+    # users = models.ManyToManyField(UserManager, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Goodside(models.Model):
     content = models.TextField(max_length=500)
-    created = models.DateTimeField(auto_now_add=True)
+    users = models.ManyToManyField(User, blank=True)
+    #created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
+
 
 class Badside(models.Model):
-    content = models.TextField(verbose_name="ürün yorumu",max_length=500)
-    content = models.TextField(max_length=500)
+    content = models.TextField(verbose_name="ürün yorumu", max_length=500)
+    users = models.ManyToManyField(User, blank=True)
+    # created = models.DateTimeField(auto_now_add=True)
 
-
+    def __str__(self):
+        return self.content
